6f02e47b6d7d2da565b4fca860ead406b79e58fe
James Cordoba <stiven.cor2002@gmail.com>
2020-02-25 19:45:34 -0500

    Iniciando proyecto

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+    function insercionDirecta($A,$n)
+    {
+        for ($i = 1; $i < $n; $i++)
+        {
+          $v = $A[$i];
+          $j = $i - 1;
+          while ($j >= 0 && $A[$j] > $v)
+          {
+            $A[$j + 1] = $A[$j];
+            $j--;
+          }
+
+          $A[$j + 1] = $v;
+        }
+        return $A;
+    }
+    function main()
+    {
+        $VectorA=array(5,4,3,2,1);
+        echo "Vector desordenado: ";
+        for($i=0;$i<sizeof($VectorA);$i++)
+        {
+          echo $VectorA[$i]."\n";
+        }
+        echo "<br>";
+        $VectorB=insercionDirecta($VectorA,sizeof($VectorA));
+        echo "Vector ordenado: ";
+        for($i=0;$i<sizeof($VectorB);$i++)
+        {
+          echo $VectorB[$i]."\n";
+        }
+    }
+    main();
+?>
